2481b26dd650c478f24c6ca47b0920784683f508
aleksey <kvyatkovsky89@gmail.com>
2025-10-27 00:27:05 +0300

    Remove unnecessary comments

--- src/scripts/test_reciever_topic.sh
+++ src/scripts/test_reciever_topic.sh
@@ -9,11 +9,7 @@ CERT_DIR="/data"
 CERT_URL="https://storage.yandexcloud.net/cloud-certs/CA.pem"
 
 BROKER="rc1b-2erh7b35n4j4v869.mdb.yandexcloud.net:9091"
-# --- ЭТО ЕДИНСТВЕННОЕ ИЗМЕНЕНИЕ ---
-# Устанавливаем ваш ВЫХОДНОЙ топик
-TOPIC="student.topic.cohort39.KvaytkovskyAleksey_out"
 
-# Скачиваем сертификат, если его нет
 if [ ! -f "$CERT_PATH" ]; then
   echo "Certificate not found at $CERT_PATH. Downloading..."
   wget "$CERT_URL" -P "$CERT_DIR"
@@ -26,7 +22,6 @@ fi
 echo "Starting Kafka consumer for topic: $TOPIC"
 echo "Press [Ctrl+C] to stop."
 
-# Флаг -C переключает kafkacat в режим потребителя (consumer)
 kafkacat -b "$BROKER" \
     -C \
     -X security.protocol=SASL_SSL \
